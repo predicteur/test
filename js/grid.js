1f4dfe38a3a82f4cc6938d9d9a004b0e9e9a4c3c
philippe <philippe.thomy@renault.com>
2020-12-30 22:05:15 +0100

    Add files via upload

--- js/grid.js
+++ js/grid.js
@@ -1,11 +1,10 @@
 // JavaScript source code
 
-function gridColor(val, min, max) {
+function gridColor(val) {
     return colors[Math.max(Math.min(Math.floor((val - min) / (max - min) * ncolor), ncolor - 1), 0)];
 }
 function gridStyle(feature) {
-    let min = 0.0, max = 1.0;
-    return { fillColor: gridColor(feature.properties.value, min, max),
+    return { fillColor: gridColor(feature.properties.value),
              weight: 0.05,
              opacity: 0.5,
              color: "black",
@@ -16,38 +15,45 @@ function gridPopup(feature, layer) {
         '<div class="popup">' +
         'top : ' + valJson.properties.top + '<br>' +
         'coordonnees : ' + feature.properties.gridcoord + '<br>' +
-        '<b>' + 'valeur : ' + feature.properties.value + '</b>' +
+        '<b>' + 'valeur : ' + (feature.properties.value * (max - min) + min).toFixed(2) + '</b>' +
         '</div>' );
 }
 function gridLegend() {
     let div = L.DomUtil.create("div", "legend");
     div.innerHTML =
-        '<b>Concentration polluant</b><br>NO2<br>' + '<small>ppm</small><br>'
-    for (let i = ncolor - 1; i > 0; i--) {
-        let i1 = i + 1;
+        '<b>Concentration polluant</b><br>' + valJson.properties.polluant + '<br>' + '<small>ppm</small><br>'
+    for (let i = ncolor - 1; i >= 0; i--) {
+        let seuilh = ((i + 1) / ncolor * (max - min) + min).toFixed(1);
+        let seuilb = (i / ncolor * (max - min) + min).toFixed(1);
         div.innerHTML +=
             '<i style="background-color: ' + colors[i] + '"></i>' +
-            i + ' - ' + i1 + '<br>';
+         seuilb + ' - ' + seuilh + '<br>';
     }
     return div;
 }
-// affichage des cartes
-//colorbrewer2.org/?type=sequential&scheme=OrRd&n=9
+//couleur : colorbrewer2.org/?type=sequential&scheme=OrRd&n=9
 let ncolor = 9;
 let colors = ['#fff7ec', '#fee8c8', '#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f', '#b30000', '#7f0000'];
-let valJson = {};
+// carte
 let map = L.map("map").setView([43.5, 5], 9);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     { attribution: '&copy; <a href="http://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a>' }
 ).addTo(map);
+// marqueurs et grille
+let max = 1.0;
+let min = 0.0;
 let layers = L.layerGroup().addTo(map);
 L.marker([43.5, 5]).addTo(layers);
+let valJson = {};
 fetch("json/geojson_petit.json")
     .then(function (response) { return response.json(); })
     .then(function (data) {
         valJson = data;
+        max = valJson.properties.valmax;
+        min = valJson.properties.valmin;
         L.geoJSON(data, { style: gridStyle, onEachFeature: gridPopup }).addTo(layers);
     })
+// légende
 let legend = L.control({ position: "topright" });
 legend.onAdd = gridLegend;
 legend.addTo(map);
